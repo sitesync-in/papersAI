26e7bc3fcb6fafee48b26126c308c12d1dfa419c
sitesync-in <info.sitesync@gmail.com>
2026-03-09 15:17:46 +0530

    fix: add STATIC_ROOT for collectstatic

--- backend/papersai/settings.py
+++ backend/papersai/settings.py
@@ -82,6 +82,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
